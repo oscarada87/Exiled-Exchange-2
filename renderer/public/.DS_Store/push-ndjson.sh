0e16863c7b9a3691a472abd665da12f31180e0e9
kvan7 <kvan.valve@gmail.com>
2024-12-15 08:49:42 -0600

    Adds starting for localization

--- renderer/public/.DS_Store/push-ndjson.sh
+++ renderer/public/.DS_Store/push-ndjson.sh
@@ -6,13 +6,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Get the data directory
-DATA_DIR="./data/en"
+DATA_DIR="./data"
 
 # Get all *.ndjson files in the data directory
 FILES=$(find $DATA_DIR -name "*.ndjson")
 
 # Loop through each file and push it to the actual data directory, overwriting any existing files
 for file in $FILES; do
-    echo "Pushing $file to data/en"
-    cp $file ../data/en
+    echo "Pushing $file to data"
+    cp $file ../data
 done
