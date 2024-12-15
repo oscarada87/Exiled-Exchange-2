3dee7841ef4a0184414781548d6202ff59dfc568
kvan7 <kvan.valve@gmail.com>
2024-12-15 12:43:04 -0600

    revert tests cause they were really terrible

--- renderer/public/.DS_Store/copy-py-ndjson.sh
+++ renderer/public/.DS_Store/copy-py-ndjson.sh
@@ -4,5 +4,5 @@ supported_languages=("en" "ru")
 
 for lang in "${supported_languages[@]}"; do
     echo "Copying $lang"
-    cp -R ./vendor/client/pyDumps/$lang/*.ndjson ./data/$lang/
+    cp -R ./vendor/client/pyDumps/$lang/* ./data/$lang/
 done
